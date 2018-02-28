cc53f46dd45d7875dc291383e5f5d15998e9d7a3
nhanquach <xomix1802@gmail.com>
2018-02-28 22:31:12 +0700

    Init

---
+++ Local/js/app.js
@@ -0,0 +1,20 @@
+var app = angular.module('app', ['ngRoute']);
+app.controller('appController', function ($scope, $route, $http) {
+    let url = "https://jsonplaceholder.typicode.com/posts";
+    
+    $scope.message = "It's working. Below is a fake API request.";
+    $http.get(url).then(data => {
+        $scope.posts = data.data;
+    })
+});
+
+//ngRoute Config
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/about', {
+            templateUrl: 'about.html'
+        })
+        .otherwise(
+            {templateUrl: 'home.html'}
+        )
+});
